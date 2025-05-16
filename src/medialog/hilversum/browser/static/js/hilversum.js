f505f67ec4b371d3928a0057aca6ccdb099eb032
Espen Moe-Nilssen <espen@medialog.no>
2025-05-16 16:32:49 +0200

    Update hilversum.js

--- src/medialog/hilversum/browser/static/js/hilversum.js
+++ src/medialog/hilversum/browser/static/js/hilversum.js
@@ -1,21 +1,14 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     const toggle = document.getElementById('filter-toggle');
     const filterSection = document.getElementById('filter-section');
     const icon = document.getElementById('filter-icon');
-    // Check if collectionfilter is in url
-    const fullUrl = window.location.href;
-    // console.log("Full URL:", fullUrl);
-    // Use URLSearchParams to extract query parameters
+
     const urlParams = new URLSearchParams(window.location.search);
     const collectionFilter = urlParams.get("collectionfilter");
-    // for (const [key, value] of urlParams.entries()) {
-    //     console.log(`${key}: ${value}`);
-    // 'dropdown-' + key = id
-    // }
 
     if (urlParams.has("collectionfilter")) {
         filterSection.classList.toggle('hidden');
+        icon.textContent = filterSection.classList.contains('hidden') ? '▶' : '▼';
     }
 
     toggle.addEventListener('click', function () {
@@ -23,30 +16,20 @@ document.addEventListener('DOMContentLoaded', function () {
         icon.textContent = filterSection.classList.contains('hidden') ? '▶' : '▼';
     });
 
-    var selects = document.querySelectorAll('select[id^="dropdown-"]');
-
-
-    // Parse URL parameters
-    const params = new URLSearchParams(window.location.search);
+    const selects = document.querySelectorAll('select[id^="dropdown-"]');
 
     selects.forEach(function (select) {
-        
-        // Bind change handler for AJAX loading
         select.addEventListener('change', function () {
-            var item = this.value;
-            var key = select.id;            
-
-            const dropdown = document.getElementById(key);
-            select.classList.remove('enabled');
-                
+            const item = this.value;
+            // const key = select.id;
+            var key = this.id.replace('dropdown-', '');
 
             if (key && item) {
-                // If this key is present in the URL, add 'enabled' class            
-                const dropdown = document.getElementById(key);
                 select.classList.add('enabled');
                 const siteUrl = document.body.dataset.portalUrl;
                 const baseUrl = siteUrl + '/prolong-collection';
-                const query = 'collectionfilter=1&' + encodeURIComponent(key) + '=' + encodeURIComponent(item);
+                
+                const query = 'collectionfilter=1&' + encodeURIComponent(key) + '=' + encodeURIComponent(item.replace(/^ /, ''));
                 const url = baseUrl + '?' + query;
                 
 
