97676bfc08c82510b4cdfadc94fe4214e4772f14
Espen Moe-Nilssen <espen@medialog.no>
2025-05-16 12:23:01 +0200

    Switch to plain javascript for filters

--- src/medialog/hilversum/browser/static/js/hilversum.js
+++ src/medialog/hilversum/browser/static/js/hilversum.js
@@ -1,19 +1,4 @@
 
-$(document).ready(function () {
-    $('select[id^="dropdown-"]').on('change', function () {
-        var key = this.id.replace('dropdown-', '');
-        var item = $(this).val();
-
-        if (key && item) {
-            var baseUrl = '${portal_url}/prolong-collection/';
-            var query = 'collectionfilter=1&' + encodeURIComponent(key) + '=' + encodeURIComponent(item) + '&discipline_op=or';
-            window.location.href = baseUrl + '?' + query;
-        }
-    });
-});
-
- 
-
 document.addEventListener('DOMContentLoaded', function () {
     const toggle = document.getElementById('filter-toggle');
     const filterSection = document.getElementById('filter-section');
@@ -23,4 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
         filterSection.classList.toggle('hidden');
         icon.textContent = filterSection.classList.contains('hidden') ? '▶' : '▼';
     });
-});
+
+    var selects = document.querySelectorAll('select[id^="dropdown-"]');
+  
+    selects.forEach(function (select) {
+      select.addEventListener('change', function () {
+        var key = this.id.replace('dropdown-', '');
+        var item = this.value;
+  
+        if (key && item) {
+          var siteUrl = document.body.dataset.portalUrl;
+          var baseUrl = siteUrl + '/prolong-collection/';
+          var query = 'collectionfilter=1&' + encodeURIComponent(key) + '=' + encodeURIComponent(item) + '&discipline_op=or';
+          window.location.href = baseUrl + '?' + query;
+        }
+      });
+    });
+  });
