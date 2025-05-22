78b324d8c1b5c22f1c8216a9a18af3076f14140b
Espen Moe-Nilssen <espen@medialog.no>
2025-05-22 20:37:47 +0200

    Add batch

--- src/medialog/hilversum/views/proloog_listing.py
+++ src/medialog/hilversum/views/proloog_listing.py
@@ -15,6 +15,8 @@ class ProloogListing(CollectionView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('proloog_listing.pt')
+    # b_size  = 100
+    
 
     def __call__(self):
         # Implement your own actions:
