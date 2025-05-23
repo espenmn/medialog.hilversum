383f7293cc7cbb127f7ad0e16b6c9dcef8404493
Espen <espen@medialog.no>
2025-05-23 12:33:55 +0200

    "Add vocabulary: PrologKeywords"

--- src/medialog/hilversum/portlets/prolongfilter.py
+++ src/medialog/hilversum/portlets/prolongfilter.py
@@ -10,19 +10,23 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from z3c.form import field
 from zope.component import getMultiAdapter
 from zope.interface import implementer
+from zope.interface import Interface
+from Products.CMFCore.utils import getToolByName
+from plone import api
 
-import json
-import six.moves.urllib.error
-import six.moves.urllib.parse
-import six.moves.urllib.request
+
+from medialog.hilversum.keywords import get_keywords as keywords
+# from medialog.hilversum.keywords import get_discipline as discipline
+from urllib.parse import unquote
 
 
 class IProlongfilterPortlet(IPortletDataProvider):
-    place_str = schema.TextLine(
-        title=_(u'Name of your place with country code'),
-        description=_(u'City name along with country code i.e Delhi,IN'),  # NOQA: E501
-        required=True,
-        default=u'delhi,in'
+    replace_fields = schema.List(
+        title=u"Select Proloog Fields to overwrite",
+        description=u"Select the fields to override.",
+        default=['naam', 'omschrijving'],
+        required=False,
+        value_type=schema.Choice(vocabulary='medialog.hilversum.ProloogFields')
     )
 
 
@@ -30,31 +34,31 @@ class IProlongfilterPortlet(IPortletDataProvider):
 class Assignment(base.Assignment):
     schema = IProlongfilterPortlet
 
-    def __init__(self, place_str='delhi,in'):
-        self.place_str = place_str.lower()
+    def __init__(self, replace_fields=[]):
+        self.replace_fields = replace_fields
 
     @property
     def title(self):
-        return _(u'Weather of the place')
+        return _(u'Filters')
 
 
 class AddForm(base.AddForm):
     schema = IProlongfilterPortlet
     form_fields = field.Fields(IProlongfilterPortlet)
-    label = _(u'Add Place weather')
-    description = _(u'This portlet displays weather of the place.')
+    label = _(u'Add Filters')
+    description = _(u'This portlet displays Filters.')
 
-    def create(self, data):
-        return Assignment(
-            place_str=data.get('place_str', 'delhi,in'),
-        )
+    # def create(self, data):
+    #     return Assignment(
+    #         replace_fields.get('place_str', 'delhi,in'),
+    #     )
 
 
 class EditForm(base.EditForm):
     schema = IProlongfilterPortlet
     form_fields = field.Fields(IProlongfilterPortlet)
-    label = _(u'Edit Place weather')
-    description = _(u'This portlet displays weather of the place.')
+    label = _(u'Edit Filters')
+    description = _(u'This portlet displays filters')
 
 
 class Renderer(base.Renderer):
@@ -68,7 +72,11 @@ class Renderer(base.Renderer):
             (context, self.request),
             name=u'plone_portal_state'
         )
+        self.keywords = keywords(self)
+        self.site_url = self.site_url()
+        # self.discipline = discipline()
         self.anonymous = portal_state.anonymous()
+        self.keyword = self.get_keyword()
 
     def render(self):
         return self._template()
@@ -77,31 +85,21 @@ class Renderer(base.Renderer):
     def available(self):
         """Show the portlet only if there are one or more elements and
         not an anonymous user."""
-        return not self.anonymous and self._data()
+        return not self.anonymous and self.keyword
 
-    def weather_report(self):
-        self.result = self._data()
-        return self.result['description']
+    def site_url(self):
+        return api.portal.get().absolute_url()
+ 
+    @memoize   
+    def get_keyword(self):
+        filters = []
+        context = self.context
+        catalog = getToolByName(context, 'portal_catalog')        
+        for keyword in self.keywords:          
+            # Get unique values from  index
+            index = catalog._catalog.getIndex(keyword[0])
+            if hasattr(index, 'uniqueValues'):
+                filters.append({'name': keyword[0], 'title' : keyword[1], 'vals': sorted(index.uniqueValues())})            
+        return filters
 
-    def get_humidity(self):
-        return self.result['humidity']
-
-    def get_pressure(self):
-        return self.result['pressure']
-
-    @memoize
-    def _data(self):
-        baseurl = 'https://query.yahooapis.com/v1/public/yql?'
-        yql_query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="{0}")'.format(  # NOQA: E501
-            self.data.place_str,
-        )
-        yql_url = baseurl + six.moves.urllib.parse.urlencode(
-            {'q': yql_query},
-        ) + '&format=json'
-        result = six.moves.urllib.request.urlopen(yql_url).read()
-        data = json.loads(result)
-        result = {}
-        result['description'] = data['query']['results']['channel']['description']  # NOQA: E501
-        result['pressure'] = data['query']['results']['channel']['atmosphere']['pressure']  # NOQA: E501
-        result['humidity'] = data['query']['results']['channel']['atmosphere']['humidity']  # NOQA: E501
-        return result
+ 
