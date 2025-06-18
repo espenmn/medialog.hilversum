d62efef5bfbb462d6c1b8b1553948128656b1cb7
Espen Moe-Nilssen <espen@medialog.no>
2025-06-18 10:32:41 +0200

    Update aanbieder.py

--- src/medialog/hilversum/content/aanbieder.py
+++ src/medialog/hilversum/content/aanbieder.py
@@ -11,15 +11,16 @@ from medialog.hilversum import _
 class IAanbieder(model.Schema):
     """ Marker interface for Aanbieder
     """
+    infotext = RichText(
+        title=u"Infotext",
+        required=False
+    )
 
 @implementer(IAanbieder)
 class Aanbieder(Item):
     """ Content-type class for Aanbieder
     """
-    infotext = RichText(
-        title=u"Infotext",
-        required=False
-    )
+   
  
 # firstletter = schema.TextLine(
 #     title=u"For the index",
