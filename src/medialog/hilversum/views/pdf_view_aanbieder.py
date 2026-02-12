6045de05cd3c5aa95fbf6da361c16ec5e8fce0a1
Espen Moe-Nilssen <espen@medialog.no>
2026-02-12 15:34:51 +0100

    First aanbieder pdf view

---
+++ src/medialog/hilversum/views/pdf_view_aanbieder.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# from medialog.hilversum import _
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+from weasyprint import HTML
+from plone import api
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from medialog.hilversum.views.aanbieder_view import AanbiederView
+
+class IPDFView(Interface):
+    """ Marker Interface for IPDFView"""
+
+
+@implementer(IPDFView)
+class PDFView(AanbiederView):
+    # We might want to define different templates for different content types (?)
+    # template = ViewPageTemplateFile('pdf_view.pt')
+
+    def __call__(self):
+
+        # Render a dedicated template for PDF
+        # set in configure.zcml, use template if we need 'options'
+        
+        self.discipline_images = self.get_discipline_images()
+        self.portal_url = self.get_portal_url()
+        self.courses = self.get_courses()
+        self.disciplines = self.get_disciplines()
+        
+        html = self.index()
+
+        pdf = HTML(
+            string=html,
+            base_url=self.context.absolute_url()
+        ).write_pdf()
+
+        self.request.response.setHeader(
+            "Content-Type",
+            "application/pdf",
+        )
+
+        self.request.response.setHeader(
+            "Content-Disposition",
+            f'attachment; filename="{self.context.id}.pdf"',
+        )
+
+        return pdf
