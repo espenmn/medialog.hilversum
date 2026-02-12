c3e3371faa5af1b07795ff99036bd036080a3263
Espen Moe-Nilssen <espen@medialog.no>
2026-02-12 14:52:25 +0100

    Added print template with 3 alternatives

--- src/medialog/hilversum/views/pdf_view.py
+++ src/medialog/hilversum/views/pdf_view.py
@@ -6,31 +6,23 @@ from zope.interface import implementer
 from zope.interface import Interface
 from weasyprint import HTML
 from plone import api
-
-
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from medialog.hilversum.views.proloog_view import ProloogView
 
 class IPDFView(Interface):
     """ Marker Interface for IPDFView"""
 
 
 @implementer(IPDFView)
-class PDFView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
+class PDFView(ProloogView):
+    # We might want to define different templates for different content types (?)
     # template = ViewPageTemplateFile('pdf_view.pt')
-
-    def __call__(self):
-        # Implement your own actions:
-        return self.index()
-
-
-
-class AsPDF(BrowserView):
 
     def __call__(self):
 
         # Render a dedicated template for PDF
+        # set in configure.zcml, use template if we need 'options'
         html = self.index()
 
         pdf = HTML(
